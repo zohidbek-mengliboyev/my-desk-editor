809c561ae2a7fd62267566ffda29da53ac33b919
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-09 01:47:39 +0500

    Adding a WYSIWYG editor library for Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxWigModule } from 'ngx-wig';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWigModule
   ],
   providers: [],
   bootstrap: [AppComponent]
